b17cd10e5c586416da095508bfee94e0183eb3b3
Himanshu Singh <hsnice16@gmail.com>
2024-10-05 20:54:50 +0530

    changes for v0.1.0

--- core/service/util_service.go
+++ core/service/util_service.go
@@ -1,7 +1,11 @@
+// Package service provides core services for verifying an email address.
 package service
 
 import "regexp"
 
+// VerifyEmail verifies if the passed email string argument is
+// a valid email or not. It returns the bool status and any
+// error encountered.
 func VerifyEmail(email string) (bool, error) {
 	validateEmailRegexString := `^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$`
 	matched, err := regexp.MatchString(validateEmailRegexString, email)
